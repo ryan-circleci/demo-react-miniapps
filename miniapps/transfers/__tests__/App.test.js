59f96a0efed448e84a2e181a9bfaab0a741c16af
Ryan E. Hamilton <ryan.hamilton@circleci.com>
2026-06-17 15:48:13 -0400

    bench: phase2 layered seed (lint + payments test + transfers test)

--- miniapps/transfers/__tests__/App.test.js
+++ miniapps/transfers/__tests__/App.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import App from '../src/App';
 
-test('renders Transfers title', () => {
+test('renders Transfers title and welcome subtitle', () => {
   const { getByText } = render(<App />);
   expect(getByText('Transfers')).toBeTruthy();
+  expect(getByText('Welcome back, Alex')).toBeTruthy();
 });
